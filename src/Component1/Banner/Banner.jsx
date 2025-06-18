d962f501626a2562c1449e595d9ea2ae20e27646
jisan5620jk <jisank5620@gmail.com>
2025-06-18 05:02:08 -0700

    Update Banner component

--- src/Component1/Banner/Banner.jsx
+++ src/Component1/Banner/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
   return (
     <section className="bg-SecondaryColor-0 px-8 pt-8 flex items-center relative z-10 overflow-hidden">
       <div className="w-full grid lg:grid-cols-2 items-center gap-8">
-        <div className="bg-BodyBgDark-0 flex justify-center px-[100px] pb-[120px] pt-20 rounded-[20px] relative overflow-hidden">
+        <div className="bg-BodyBgDark-0 flex justify-center px-[100px] pb-[120px] pt-[126px] rounded-[20px] relative overflow-hidden">
           <div>
             <img
               src={bannerImg}
@@ -44,7 +44,7 @@ const Banner = () => {
             className="absolute bottom-[60px] animate-rotate hidden md:block lg:hidden 2xl:block"
           />
         </div>
-        <div className="bg-gradient-to-tl from-BodyBg-0 via-BoxBg2-0 to-BoxBg3-0 px-[100px] pt-[245px] pb-20 rounded-[20px] relative overflow-hidden">
+        <div className="bg-gradient-to-tl from-BodyBg-0 via-BoxBg2-0 to-BoxBg3-0 px-[100px] pt-[228px] pb-[102px] rounded-[20px] relative overflow-hidden">
           <h1 className="font-Outfit font-semibold text-HeadingColor-0 text-[26px] leading-[32px] sm:text-[36px] sm:leading-[42px] md:text-[56px] md:leading-[62px] lg:text-[42px] lg:leading-[48px] xl:text-[48px] xl:leading-[76px] 2xl:text-[74px] 2xl:leading-[78px]">
             Building evergreen
             <br />
@@ -52,11 +52,37 @@ const Banner = () => {
             <br />
             <span className="text-PrimaryColor-0">Innovate</span> earth
           </h1>
-          <div className="mt-8">
+          <div className="mt-10 flex items-center gap-7">
             <PrimaryButton as="link" href="/about">
               Explore Services <GoArrowRight size={22} />
             </PrimaryButton>
-            <div></div>
+            <div>
+              <h6 className="font-NotoSans text-[15px] text-TextColor-0 mb-1">
+                Call Us (Toll Free)
+              </h6>
+              <a
+                href="calto:+1456678032"
+                title="+1 (456) 678 032"
+                className="font-Outfit text-[22px] font-medium text-HeadingColor-0"
+              >
+                +1 (456) 678 032
+              </a>
+            </div>
+          </div>
+          <div className="flex justify-between mt-[136px] pt-5 border-t border-SecondaryColor-0">
+            <div>
+              <h5 className="font-Outfit font-medium text-HeadingColor-0 text-lg leading-7">
+                We’re Since <span className="text-PrimaryColor-0">1998</span>
+                <br />
+                Based in USA
+              </h5>
+            </div>
+            <div>
+              <p className="font-NotoSans text-TextColor-0 max-w-[390px]">
+                Econet is a nonprofit environmental organizations dedicated to
+                protecting planet through sustainable community empowerment.
+              </p>
+            </div>
           </div>
 
           <div className="absolute z-10 top-5 right-0 hidden md:block lg:hidden 2xl:block">
